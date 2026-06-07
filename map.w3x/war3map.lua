e5706ffc54c9ccd5c019f2be4e5d9cfacca2a90e
VinerX <60321176+VinerX@users.noreply.github.com>
2026-06-07 10:24:24 +0300

    Scarlet ability data (attackerData + attackedData + getLvlData, replace procedural)

--- map.w3x/war3map.lua
+++ map.w3x/war3map.lua
@@ -57514,14 +57514,67 @@ RegisterAiRace("Scarlet", {
             { at = 60, action = "mageTp" },
         },
     },
+    attackerData = {
+        [FourCC('H06C')] = {
+            { order = "firebolt", chance = 5, type = "target" },
+            { order = "flamestrike", chance = 5, type = "point" },
+            { order = "waterelemental", chance = 5, type = "immediate" },
+        },
+        [FourCC('H06B')] = {
+            { order = "berserk", chance = 5, type = "immediate" },
+            { order = "thunderclap", chance = 5, type = "immediate" },
+            { order = "roar", chance = 5, type = "immediate" },
+        },
+        [FourCC('H03H')] = {
+            { order = "firebolt", chance = 5, type = "heal", allyRange = 450 },
+            { order = "roar", chance = 5, type = "immediate" },
+            { order = "resurrection", chance = 5, type = "immediate" },
+        },
+        [FourCC('h067')] = {
+            { order = "flamingarrows", chance = 4, type = "target" },
+            { order = "devourmagic", chance = 4, type = "target", range = 525 },
+        },
+        [FourCC('n008')] = {
+            { order = "flamingarrows", chance = 4, type = "target" },
+            { order = "dispel", chance = 4, type = "point", range = 490 },
+        },
+        [FourCC('h03G')] = {
+            { order = "berserk", chance = 3, type = "target" },
+            { order = "roar", chance = 3, type = "point" },
+        },
+        [FourCC('h00Z')] = {
+            { order = "ancestralspirit", chance = 6, type = "target", range = 125 },
+            { order = "clusterrockets", chance = 6, type = "point", range = 300 },
+            { order = "tranquility", chance = 6, type = "immediate", hp = 45 },
+        },
+        [FourCC('h00Y')] = {
+            { order = "ancestralspirit", chance = 6, type = "target", range = 125 },
+            { order = "clusterrockets", chance = 6, type = "point", range = 300 },
+            { order = "tranquility", chance = 6, type = "immediate", hp = 45 },
+        },
+    },
+    attackedData = {
+        [FourCC('h039')] = {
+            { order = "defend", chance = 5, type = "immediate" },
+            { order = "magicdefense", chance = 5, type = "immediate" },
+            { order = "undefend", chance = 5, type = "immediate" },
+            { order = "magicundefense", chance = 5, type = "immediate" },
+        },
+        [FourCC('h03B')] = {
+            { order = "defend", chance = 3, type = "immediate" },
+            { order = "undefend", chance = 3, type = "immediate" },
+        },
+    },
+    getLvlData = {
+        [FourCC('H06C')] = { ult = FourCC('A09M'), skills = { FourCC('A09K'), FourCC('A09L'), FourCC('A09N') } },
+        [FourCC('H06B')] = { ult = FourCC('A09J'), skills = { FourCC('A09F'), FourCC('A09G'), FourCC('A09I') } },
+        [FourCC('H03H')] = { ult = FourCC('A09E'), skills = { FourCC('A097'), FourCC('A09C'), FourCC('A09D') } },
+    },
     chooseBuild = ChooseBuildings_ScarletOrden,
     perebor = PereborBuildings_ScarletOrden,
     join = Join_Skarlet,
     strateg = Strateg_Scarlet,
     strategEC = Strateg_Scarlet_EC,
-    attacker = Attacker_Skarlet,
-    attacked = AttackedScarlet,
-    getLvl = GetLvlScarlet,
     upgrade = UpgradeScarlet,
     naval = aiNavalTrain_Common,
     wall = FourCC('h011'),
